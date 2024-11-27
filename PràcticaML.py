45ff2f7a6716584b370c155613b65158bb8fc128
adrianfuuster <1665211@uab.cat>
2024-11-27 17:41:44 +0100

    filter_dalc_column

--- PràcticaML.py
+++ PràcticaML.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+
+df= pd.read_csv("student-mat.csv")
+df.drop("Dalc",axis=1,inplace=True)
+
+
+# Dividir el DataFrame
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+
